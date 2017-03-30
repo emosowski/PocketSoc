0f0630b987aee4a5d757017e5ea6c00c4a0bfbf4
Osowski <osowskie@E0368095.local>
2017-03-30 17:01:14 -0400

    Update UI

--- Security App/alpharettaMapViewController.swift
+++ Security App/alpharettaMapViewController.swift
@@ -6,4 +6,105 @@
 //  Copyright © 2017 emilyosowski. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import MapKit
+
+class alpharettaMapViewViewController: UIViewController, MKMapViewDelegate {
+    
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
+    //    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var tempLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    //    @IBOutlet weak var weatherImage: UIImageView!
+    @IBOutlet weak var weatherLabel: UILabel!
+    
+    var weather = alpharettaWeatherDataModel()
+    
+    let regionRadius: CLLocationDistance = 2000
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.parent?.title = "PocketSoc"
+        
+        let initialLocation = CLLocation(latitude: 34.089799, longitude: -84.265930)
+        centerMapOnLocation(initialLocation)
+        
+        mapView.delegate = self
+        
+        let location = Location(title: "Vodafone Alpharetta",
+                                locationName: "12725 Morris Road Ext, Suite 450",
+                                discipline: "Office",
+                                coordinate: CLLocationCoordinate2D(latitude: 34.089799, longitude: -84.265930))
+        
+        
+        mapView.addAnnotation(location)
+        
+        let yourAnnotationAtIndex = 0
+        mapView.selectAnnotation(mapView.annotations[yourAnnotationAtIndex], animated: true)
+        //automatically displays annotation info without tap
+        
+        weather.downloadData {
+            self.updateUI()
+        }
+        
+        
+        
+        
+        
+        
+    }
+    
+    //    override func viewWillAppear(_ animated: Bool) {
+    //        super.viewWillAppear(animated)
+    //        self.tabBarController?.tabBar.tintColor = UIColor.black
+    //    }
+    
+    
+    func mapView(_ mapView: MKMapView,
+                 viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        if let annotation = annotation as? Location {
+            let identifier = "artPin"
+            var view: MKPinAnnotationView
+            if let dequeuedView = mapView.dequeueReusableAnnotationView(withIdentifier: identifier)
+                as? MKPinAnnotationView { // 2
+                dequeuedView.annotation = annotation
+                view = dequeuedView
+            } else {
+                // 3
+                view = MKPinAnnotationView(annotation: annotation, reuseIdentifier: identifier)
+                view.canShowCallout = true
+                view.calloutOffset = CGPoint(x: -5, y: 5)
+                view.rightCalloutAccessoryView = UIButton(type: .detailDisclosure) as UIView
+            }
+            
+            return view
+        }
+        return nil
+    }
+    
+    func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
+        let location = view.annotation as! Location
+        let launchOptions = [MKLaunchOptionsDirectionsModeKey: MKLaunchOptionsDirectionsModeDriving]
+        location.mapItem().openInMaps(launchOptions: launchOptions)
+    }
+    
+    
+    func centerMapOnLocation(_ location: CLLocation) {
+        let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate,
+                                                                  regionRadius * 2.0, regionRadius * 2.0)
+        mapView.setRegion(coordinateRegion, animated: true)
+    }
+    
+    func updateUI() {
+        //        dateLabel.text = weather.date
+        tempLabel.text = "\(weather.temp)"
+        locationLabel.text = weather.location
+        weatherLabel.text = weather.weather
+        //        weatherImage.image = UIImage(named: weather.weather)
+    }
+    
+    
+}
+
---
+++ Security App/denverContactViewController.swift
@@ -0,0 +1,123 @@
+//
+//  denverContactViewController.swift
+//  Security App
+//
+//  Created by Osowski, Emily, Vodafone Americas on 3/27/17.
+//  Copyright © 2017 emilyosowski. All rights reserved.
+//
+
+import UIKit
+import MessageUI
+
+class denverContactViewController: UIViewController, MFMailComposeViewControllerDelegate {
+    
+    
+    
+    
+
+    @IBAction func officeCall(_ sender: Any) {
+        let number = URL(string: "telprompt://3038985417")!
+        if #available(iOS 10.0, *) {
+            UIApplication.shared.open(number, options: [:], completionHandler: nil)
+        } else {
+            UIApplication.shared.openURL(number)
+        }
+    }
+    
+    
+    //    @IBAction func sosCall(_ sender: Any) {
+    //
+    //        let number = URL(string: "telprompt://44(0)2087628021")!
+    //        if #available(iOS 10.0, *) {
+    //            UIApplication.shared.open(number, options: [:], completionHandler: nil)
+    //        } else {
+    //            UIApplication.shared.openURL(number)
+    //        }
+    //    }
+    
+    @IBAction func sosAppLaunch(_ sender: Any) {
+        if let url = NSURL(string: "isosassistance://"), UIApplication.shared.canOpenURL(url as URL) {
+            //            UIApplication.shared.openURL(url as URL)
+            UIApplication.shared.open(url as URL, options: [:], completionHandler: nil)
+        } else if let itunesUrl = NSURL(string: "https://itunes.apple.com/us/app/international-sos-assistance/id465662561?mt=8"), UIApplication.shared.canOpenURL(itunesUrl as URL) {
+            //            UIApplication.shared.openURL(itunesUrl as URL)
+            UIApplication.shared.open(itunesUrl as URL, options: [:], completionHandler: nil)
+        }
+    }
+    
+    
+
+    @IBAction func helpDeskCall(_ sender: Any) {
+        
+        let number = URL(string: "telprompt://18667729350")!
+        if #available(iOS 10.0, *) {
+            UIApplication.shared.open(number, options: [:], completionHandler: nil)
+        } else {
+            UIApplication.shared.openURL(number)
+        }
+    }
+    
+    @IBAction func helpDeskEmail(_ sender: Any) {
+        
+        let mailVC = MFMailComposeViewController()
+        mailVC.mailComposeDelegate = self
+        mailVC.setToRecipients(["help.us@vodafone.com"])
+        mailVC.setSubject("")
+        mailVC.setMessageBody("", isHTML: false)
+        
+        present(mailVC, animated: true, completion: nil)
+        
+    }
+    
+    @IBAction func cwtCall(_ sender: Any) {
+        
+        let number = URL(string: "telprompt://8002535095")!
+        if #available(iOS 10.0, *) {
+            UIApplication.shared.open(number, options: [:], completionHandler: nil)
+        } else {
+            UIApplication.shared.openURL(number)
+        }
+    }
+    
+    @IBAction func cwtAppLaunch(_ sender: Any) {
+        if let url = NSURL(string: "cwttogolaunch://"), UIApplication.shared.canOpenURL(url as URL) {
+            //            UIApplication.shared.openURL(url as URL)
+            UIApplication.shared.open(url as URL, options: [:], completionHandler: nil)
+        } else if let itunesUrl = NSURL(string: "https://itunes.apple.com/us/app/cwt-to-go/id634309682?mt=8"), UIApplication.shared.canOpenURL(itunesUrl as URL) {
+            //            UIApplication.shared.openURL(itunesUrl as URL)
+            UIApplication.shared.open(itunesUrl as URL, options: [:], completionHandler: nil)
+        }
+    }
+    
+    
+    
+    
+    
+    @IBAction func evacuationPlan(_ sender: Any) {
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?) {
+        
+        // Dismiss the mail compose view controller.
+        controller.dismiss(animated: true, completion: nil)
+    }
+    
+    
+    
+    
+}
+
